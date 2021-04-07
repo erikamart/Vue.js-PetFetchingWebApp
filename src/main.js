d6b4777c6c118bdc239f21d0080f9558a52f1da5
Erika Martinez <17046460+erikamart@users.noreply.github.com>
2021-04-06 19:34:13 -0700

    Completed App

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import App from "./App";
-import Vuetify from "vuetify";
-
-Vue.use(Vuetify);
-Vue.config.productionTip = false;
+import vuetify from "@/plugins/vuetify";
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  components: { App },
-  template: "<App/>"
-});
+  vuetify,
+  render: (h) => h(App)
+}).$mount("#app");
